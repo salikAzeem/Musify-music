934d501c4b4e7895b91a44cd21b3e21ade4c2dbc
salik azeem <salikazeem123@gmail.com>
2025-07-05 11:50:06 +0530

    deply

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+// import './index.css';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />);
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+// ✅ Import service worker
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
+
+// ✅ Register PWA
+serviceWorkerRegistration.register();
